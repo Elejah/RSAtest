3f6fe4ee528f3d96be219fa679b66c1be03ed65c
Elejah <ilua.yurkevich.official@gmail.com>
2020-12-11 13:36:41 +0300

    new commit with files

---
+++ RSAtest/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace RSAtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptedMessage = new System.Windows.Forms.Label();
+            this.DecryptMessage = new System.Windows.Forms.Label();
+            this.RawMessage = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // encryptedMessage
+            // 
+            this.encryptedMessage.AutoSize = true;
+            this.encryptedMessage.Location = new System.Drawing.Point(273, 70);
+            this.encryptedMessage.Name = "encryptedMessage";
+            this.encryptedMessage.Size = new System.Drawing.Size(35, 13);
+            this.encryptedMessage.TabIndex = 14;
+            this.encryptedMessage.Text = "label1";
+            // 
+            // DecryptMessage
+            // 
+            this.DecryptMessage.AutoSize = true;
+            this.DecryptMessage.Location = new System.Drawing.Point(156, 208);
+            this.DecryptMessage.Name = "DecryptMessage";
+            this.DecryptMessage.Size = new System.Drawing.Size(35, 13);
+            this.DecryptMessage.TabIndex = 13;
+            this.DecryptMessage.Text = "label1";
+            // 
+            // RawMessage
+            // 
+            this.RawMessage.Location = new System.Drawing.Point(156, 63);
+            this.RawMessage.Name = "RawMessage";
+            this.RawMessage.Size = new System.Drawing.Size(100, 20);
+            this.RawMessage.TabIndex = 12;
+            this.RawMessage.Text = "test";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 71);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 71);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 379);
+            this.Controls.Add(this.encryptedMessage);
+            this.Controls.Add(this.DecryptMessage);
+            this.Controls.Add(this.RawMessage);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label encryptedMessage;
+        private System.Windows.Forms.Label DecryptMessage;
+        private System.Windows.Forms.TextBox RawMessage;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RSAtest/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSAtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string encryptedText = " ";
+        string privateKeyString;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var cryptoServiceProvider = new RSACryptoServiceProvider(2048); //2048 - Długość klucza
+            var privateKey = cryptoServiceProvider.ExportParameters(true); //Generowanie klucza prywatnego
+            var publicKey = cryptoServiceProvider.ExportParameters(false); //Generowanie klucza publiczny
+
+
+            string publicKeyString = GetKeyString(publicKey);
+            privateKeyString = GetKeyString(privateKey);
+
+            encryptedText = Encrypt(RawMessage.Text, publicKeyString);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string decryptedText = Decrypt(encryptedText, privateKeyString);
+            DecryptMessage.Text = decryptedText.ToString();
+        }
+
+
+        public static string GetKeyString(RSAParameters publicKey)
+        {
+
+            var stringWriter = new System.IO.StringWriter();
+            var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            xmlSerializer.Serialize(stringWriter, publicKey);
+            return stringWriter.ToString();
+        }
+
+        public static string Encrypt(string textToEncrypt, string publicKeyString)
+        {
+            var bytesToEncrypt = Encoding.UTF8.GetBytes(textToEncrypt);
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    rsa.FromXmlString(publicKeyString.ToString());
+                    var encryptedData = rsa.Encrypt(bytesToEncrypt, true);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    return base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        public static string Decrypt(string textToDecrypt, string privateKeyString)
+        {
+            var bytesToDescrypt = Encoding.UTF8.GetBytes(textToDecrypt);
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+
+                    // server decrypting data with private key                    
+                    rsa.FromXmlString(privateKeyString);
+
+                    var resultBytes = Convert.FromBase64String(textToDecrypt);
+                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                    return decryptedData.ToString();
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+    }
+}
